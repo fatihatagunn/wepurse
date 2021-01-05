32cfd543c19e25ecce1e57d8799aae682e882137
iremsenacelik <iremsbet13@gmail.com>
2021-01-05 21:06:26 +0300

    first design of create_account_page

---
+++ lib/ui/create_account_page.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:wepurseapp/ui/appbar_widget.dart';
+import 'my_home_page.dart';
+
+class CreateAccountPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CreateAccountPageState();
+  }
+}
+
+class CreateAccountPageState extends State<CreateAccountPage> {
+  String _userName, _userMail;
+  bool autoControl = false;
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          primaryColor: Colors.pink.shade200,
+          hintColor: Colors.black54,
+          accentColor: Colors.pink,
+          cursorColor: Colors.pink,
+          errorColor: Colors.red,
+        ),
+        child: Scaffold(
+          appBar: AppBarWidget(),
+          backgroundColor: Colors.white,
+          body: Padding(
+            padding: EdgeInsets.all(20),
+            child: Form(
+              key: formKey,
+              autovalidate: autoControl,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Kullanıcı Adı ",
+                      hintText: "Kullanıcı adınızı giriniz.",
+                      prefixIcon: Icon(Icons.account_circle_outlined),
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: _inputNameControl,
+                    onSaved: (inputData) => _userName = inputData,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: "E-posta",
+                      hintText: "Gecerli bir e-posta giriniz.",
+                      prefixIcon: Icon(Icons.email_outlined),
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: _inputMailControl,
+                    onSaved: (inputData) => _userMail = inputData,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      "kaydet",
+                    ),
+                    color: Colors.pink.shade200,
+                    disabledColor: Colors.pinkAccent,
+                    onPressed: () {
+                      _inputDataControl();
+                      if (_userName != null && _userMail != null) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MyHomePage(),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _inputDataControl() {
+    if (formKey.currentState.validate()) {
+      formKey.currentState.save();
+      // debugPrint(" $_userMail  , $_userName ...");
+    } else {
+      setState(() {
+        autoControl = true;
+      });
+    }
+  }
+
+  String _inputNameControl(String name) {
+    if (name.length < 4) {
+      return "Kullanıcı adı 4 karakterden uzun olmalı.";
+    } else
+      return null;
+  }
+
+  String _inputMailControl(String mail) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(mail))
+      return "Gecerli bir e-posta giriniz ";
+    else
+      return null;
+  }
+}
