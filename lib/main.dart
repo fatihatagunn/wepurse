574b9190911010d28b76a2bed7f3d49896bc871d
iremsenacelik <iremsbet13@gmail.com>
2021-01-09 02:04:19 +0300

    Fontfamily added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'ui/add_process_page.dart';
-import 'ui/create_account_page.dart';
+import 'ui/create_user_page.dart';
 import 'ui/my_home_page.dart';
 import 'ui/drawer_page.dart';
-
+import 'ui/add_account_page.dart';
+import 'ui/add_category_page.dart';
+import 'ui/settings_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-  String _userName = "irem sena celik";
-  String _userMail = "ssrem@rem.ii";
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CreateAccountPage(),
+      theme: ThemeData(
+        fontFamily: "UbuntuFont",
+      ),
+      home: CreateUserPage(),
     );
   }
 }
