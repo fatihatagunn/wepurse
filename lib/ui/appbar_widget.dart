fb472b6acd8cba533ed2f54304fd468406e4fc7b
iremsenacelik <iremsbet13@gmail.com>
2021-01-09 02:10:51 +0300

    Created a named-constructor for changing title in diff pages .

--- lib/ui/appbar_widget.dart
+++ lib/ui/appbar_widget.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
+  String titleVariable;
+
+  AppBarWidget() {
+    this.titleVariable = "wepurse";
+  }
+
+  AppBarWidget.withTitle(String titleName) {
+    this.titleVariable = titleName;
+  }
+
   @override
   Size get preferredSize => const Size.fromHeight(50);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      elevation: 10,
-      shadowColor: Colors.pinkAccent,
-      backgroundColor: Colors.white,
-      iconTheme: IconThemeData(color: Colors.pinkAccent),
-
-      title: Text("wepurse", style: TextStyle(color: Colors.pinkAccent),),
+      elevation: 15,
+      shadowColor: Colors.indigo,
+      backgroundColor: Colors.indigo,
+      iconTheme: IconThemeData(color: Colors.white),
+      title: Text(
+        titleVariable,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
       centerTitle: true,
-
     );
   }
 }
