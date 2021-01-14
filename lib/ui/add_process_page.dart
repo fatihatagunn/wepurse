027e14819a8193876d223197b4a208c702a4a9aa
iremsenacelik <iremsbet13@gmail.com>
2021-01-14 16:49:42 +0300

    New design for add_process_page.dart .

--- lib/ui/add_process_page.dart
+++ lib/ui/add_process_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:wepurseapp/ui/my_home_page.dart';
+import 'my_home_page.dart';
 import 'appbar_widget.dart';
 
 class AddProcessPage extends StatefulWidget {
@@ -14,6 +14,7 @@ class AddProcessPageState extends State<AddProcessPage> {
   double inputAmount = 0.0;
 
   FocusNode _focusNode;
+
 
   int maxLine = 1;
 
@@ -51,255 +52,166 @@ class AddProcessPageState extends State<AddProcessPage> {
 
   @override
   Widget build(BuildContext context) {
+    DateTime today = DateTime.now();
+    DateTime monthAgo = DateTime(
+      2021,
+      today.month - 1,
+    );
+    DateTime monthAfter = DateTime(
+      2021,
+      today.month + 1,
+    );
+
     return Material(
       child: Scaffold(
+        backgroundColor: Colors.indigo,
         resizeToAvoidBottomPadding: false,
         appBar: AppBarWidget.withTitle("İşlem Ekle/Düzenle"),
         body: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height / 3,
-                decoration: BoxDecoration(
-                  color: Colors.indigo,
-                  borderRadius: BorderRadius.circular(12),
+          padding: EdgeInsets.only(
+            left: 10,
+            top: 10,
+            right: 10,
+          ),
+          child: Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+            ),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 20,
+                    left: 20,
+                    top: 8,
+                    bottom: 5,
+                  ),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.monetization_on_outlined,
+                      ),
+                      labelText: "İşlem Tutarı",
+                      hintText: "İşlem Tutarını Giriniz",
+                    ),
+
+                  ),
                 ),
-                child: Row(
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 20,
+                    left: 20,
+                    top: 8,
+                    bottom: 5,
+                  ),
+                  child: TextField(
+                    maxLines: maxLine,
+                    maxLength: 50,
+                    maxLengthEnforced: true,
+                    autofocus: false,
+                    focusNode: _focusNode,
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.info_outline_rounded,
+                      ),
+                      labelText: "Detay ",
+                      hintText: "İşlem Detaylarını Giriniz",
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        )),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              "Kategori Seçimi ",
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                              ),
-                            ),
-                            DropdownButton<String>(
-                              iconEnabledColor: Colors.white,
-                              dropdownColor: Colors.indigo,
-                              items: categories.map((chosenCategory) {
-                                return DropdownMenuItem<String>(
-                                  child: Text(
-                                    chosenCategory,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  value: chosenCategory,
-                                );
-                              }).toList(),
-                              onChanged: (chosenData) {
-                                setState(() {
-                                  chosenCategory = chosenData;
-                                });
-                              },
-                              value: chosenCategory,
-                            ),
-                            SizedBox(
-                              height: 2,
-                            ),
-                            Text(
-                              "Hesap Seçimi",
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                              ),
-                            ),
-                            DropdownButton<String>(
-                              iconEnabledColor: Colors.white,
-                              dropdownColor: Colors.indigo,
-                              items: accounts.map((chosenAccount) {
-                                return DropdownMenuItem<String>(
-                                  child: Text(
-                                    chosenAccount,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  value: chosenAccount,
-                                );
-                              }).toList(),
-                              onChanged: (chosenData) {
-                                setState(() {
-                                  chosenAccount = chosenData;
-                                });
-                              },
-                              value: chosenAccount,
-                            ),
-                          ],
-                        ),
+                    Container(
+                      width: 120,
+                      height: 90,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "Kategori Seçimi ",
+                          ),
+                          DropdownButton<String>(
+                            items: categories.map((chosenCategory) {
+                              return DropdownMenuItem<String>(
+                                child: Text(
+                                  chosenCategory,
+                                ),
+                                value: chosenCategory,
+                              );
+                            }).toList(),
+                            onChanged: (chosenData) {
+                              setState(() {
+                                chosenCategory = chosenData;
+                              });
+                            },
+                            value: chosenCategory,
+                          ),
+                        ],
                       ),
                     ),
-                    VerticalDivider(
-                      color: Colors.black,
-                      indent: 30,
-                      endIndent: 30,
-                    ),
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(10),
-                            bottomLeft: Radius.circular(10),
+                    Container(
+                      width: 120,
+                      height: 90,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "Hesap Seçimi",
                           ),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              "İşlem Seçimi",
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                              ),
-                            ),
-                            RadioListTile<String>(
-                              activeColor: Colors.white,
-                              value: "Gelir",
-                              groupValue: process,
-                              onChanged: (value) {
-                                setState(() {
-                                  process = value;
-                                });
-                              },
-                              title: Text(
-                                "Gelir",
-                                style: TextStyle(
-                                  color: Colors.white,
+                          DropdownButton<String>(
+                            items: accounts.map((chosenAccount) {
+                              return DropdownMenuItem<String>(
+                                child: Text(
+                                  chosenAccount,
                                 ),
-                              ),
-                            ),
-                            RadioListTile<String>(
-                              activeColor: Colors.white,
-                              value: "Gider",
-                              groupValue: process,
-                              onChanged: (value) {
-                                setState(() {
-                                  process = value;
-                                });
-                              },
-                              title: Text(
-                                "Gider",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+                                value: chosenAccount,
+                              );
+                            }).toList(),
+                            onChanged: (chosenData) {
+                              setState(() {
+                                chosenAccount = chosenData;
+                              });
+                            },
+                            value: chosenAccount,
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: 5,
-                ),
-                padding: EdgeInsets.only(
-                  left: 10,
-                  top: 5,
-                  right: 20,
-                  bottom: 5,
-                ),
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height / 10,
-                decoration: BoxDecoration(
-                  color: Colors.indigo,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: TextFormField(
-                  maxLines: 1,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                    ),
-                    labelText: "İşlem Tutarını Giriniz",
-                    prefixIcon: Icon(
-                      Icons.monetization_on_outlined,
-                      color: Colors.white,
-                    ),
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.white,
-                      ),
-                    ),
+                RaisedButton(
+                  onPressed: () {
+                    showDatePicker(
+                      context: context,
+                      initialDate: today,
+                      firstDate: monthAgo,
+                      lastDate: monthAfter,
+                    ).then((chosenDate) {
+                      today = chosenDate;
+                    });
+                  },
+                  child: Text(
+                    "Tarih Seçimi",
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: 5,
-                ),
-                padding: EdgeInsets.all(10),
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height / 10,
-                decoration: BoxDecoration(
-                  color: Colors.indigo,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: RaisedButton(
-                  child: Text("Tarih Seçiniz"),
-                  onPressed: () {},
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: 5,
-                ),
-                padding: EdgeInsets.all(10),
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height / 10,
-                decoration: BoxDecoration(
-                  color: Colors.indigo,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: TextFormField(
-                  autofocus: false,
-                  focusNode: _focusNode,
-                  maxLines: maxLine,
-                  decoration: InputDecoration(
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                    ),
-                    labelText: "Açıklama Giriniz.",
-                    prefixIcon: Icon(
-                      Icons.info_outline,
-                      color: Colors.white,
-                    ),
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.white,
+          backgroundColor: Colors.indigo,
           child: Icon(
             Icons.done_outline_outlined,
-            color: Colors.indigo,
-            size: 37,
+            size: 32,
           ),
           onPressed: () {
             Navigator.pushReplacement(
